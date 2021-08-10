0b415552ad2b718a62db682d6016ec555094d30e
Satoshi-D <8383974+doisatoshi@users.noreply.github.com>
2021-08-10 17:03:10 +0900

    デフォルト値まで完了

--- src/index.js
+++ src/index.js
@@ -83,10 +83,18 @@
 // const message2 = `名前は${name}です。年齢は${age}歳です`;
 // console.log(message2);
 
-const myProfile = ["さとし", 42];
-const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です`;
-console.log(message3);
+// const myProfile = ["さとし", 42];
+// const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です`;
+// console.log(message3);
 
-const [name, age] = myProfile;
-const message4 = `名前は${name}です。年齢は${age}歳です`;
-console.log(message4);
+// const [name, age] = myProfile;
+// const message4 = `名前は${name}です。年齢は${age}歳です`;
+// console.log(message4);
+
+/**
+ * デフォルト値
+ */
+
+// const sayHello = (name = "さとし") => console.log(`こんにちわ!${name}さん!`);
+
+// sayHello();
